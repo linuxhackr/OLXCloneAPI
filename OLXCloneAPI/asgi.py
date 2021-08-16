993bf6a196226b4df132064f69fe81712cd08059
Priyanshu Kumar <nix0145@gmail.com>
2021-08-17 03:52:35 +0530

    some changes

--- OLXCloneAPI/asgi.py
+++ OLXCloneAPI/asgi.py
@@ -1,15 +1,15 @@
 import os
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OLXCloneAPI.settings')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.conf.urls import url
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from chat.consumers import ChatConsumer
 from OLXCloneAPI.token_auth import TokenAuthMiddlewareStack
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OLXCloneAPI.settings')
-django.setup()
 
 # ASGI run alongside WSGI https://medium.com/@Ritiktaneja/configuring-asgi-django-application-using-daphne-and-nginx-server-59a90456fe17
 
